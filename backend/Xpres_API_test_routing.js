8c96d19a56978f0adfe2b1b724b696a1b6a8dae4
Gabex_israel <153013936+Gabx-israel@users.noreply.github.com>
2025-10-30 23:18:02 -0500

    just adding these files inorder to test for connectivity to the database

---
+++ backend/Xpres_API_test_routing.js
@@ -0,0 +1,26 @@
+// backend/server.js
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const pool = require("./db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/api/db-check", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT NOW()");
+    res.json({ ok: true, dbTime: rows[0].now });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ ok: false, error: err.message });
+  }
+});
+
+app.use("/api/events", require("./routes/eventRoutes.js"));
+
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log("API running on", PORT));
